82428002d24e4f0bf7356e71907746b36ec2fbb7
“00Amit0” <amit836961@gmail.com>
2022-06-16 03:13:40 +0530

    Greetings

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+        }
+    }
+}
