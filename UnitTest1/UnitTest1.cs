bf278cb7b13f2d2682eb2cc74cf8019ee82792c3
“00Amit0” <amit836961@gmail.com>
2022-06-16 03:51:59 +0530

    Test Method for Default Constructor using Reflection

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -73,5 +73,42 @@ namespace UnitTest1
                 Assert.AreEqual(excepted, ex.Message);
             }
         }
+        [TestMethod]
+        [TestCategory("Invalid class Name")]
+        public void GivenInvalidClassName_ShouldThrow_MoodAnalyserException()
+        {
+            ///Arrange , Act and in last Assert
+            //giving expected result to variable 
+            string expected = "Class not Found";
+            try
+            {
+                //creating instence of class 
+                object obj = MoodAnalyserFactory.CreateMoodAnalyseMethod("MoodAnalyser.MoodAnalyzer", "MoodAnalyzer");
+            }
+            catch (CustomMoodAnalyserException ex)
+            {
+                //comparing and catching exception
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Invalid cunstructor")]
+        public void GivenClass_WhenNotProper_Constructor_ShouldThrow_MoodAnalyserException()
+        {
+            ///Arrange , Act and in last Assert
+            //giving expected result to variable 
+            string expected = "Constructor is not Found";
+            try
+            {
+                //creating instence of class 
+                object obj = MoodAnalyserFactory.CreateMoodAnalyseMethod("MoodAnalyser.MoodAnalyzer", "MoodAnalyzer");
+            }
+            catch (CustomMoodAnalyserException ex)
+            {
+                //comparing and catching exception
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
     }
 }
